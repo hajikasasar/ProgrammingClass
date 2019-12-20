bfc33ade715ae1ebaa30a100997d74171764c129
hajikasasar <habiyanlazuard@gmail.com>
2019-12-20 23:22:58 +0700

    Tugas1 pyhton Programming Class

---
+++ tugas_1.py
@@ -0,0 +1,12 @@
+A = int(input("Masukan Nilai pertama = "))
+B = int(input("Masukan Nilai kedua = "))
+C = int(input("Masukan Nilai ketiga = "))
+D = A + B + C
+E = A - B - C
+F = A * B * C
+G = A //B // C
+
+print(" %d + %d + %d = "%(A,B,C),D)
+print(" %d - %d - %d = "%(A,B,C),E)
+print(" %d x %d x %d = "%(A,B,C),F)
+print(" %d / %d / %d = "%(A,B,C),G)
